4ff0302bbe06dab53f40d6b3f48cb8a8886b52a1
Shahab Faderani <s.faderani@avm.de>
2025-09-05 15:36:14 +0200

    FEATURE: add animation to color theme toggle

--- game-hub/src/components/ColorModeSwitch/ColorModeSwitch.tsx
+++ game-hub/src/components/ColorModeSwitch/ColorModeSwitch.tsx
@@ -1,16 +1,24 @@
-import { ClientOnly, IconButton, Skeleton, Text, Flex } from "@chakra-ui/react";
+import { Flex, IconButton, Text, Skeleton, ClientOnly } from "@chakra-ui/react";
 import { useColorMode } from "@/components/ui/color-mode";
 import { LuMoon, LuSun } from "react-icons/lu";
+import styles from "./ColorModeSwitch.module.css";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <Flex p={4} align="center" gap={3}>
       <ClientOnly fallback={<Skeleton boxSize="8" />}>
-        <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+        <IconButton
+          onClick={toggleColorMode}
+          variant="outline"
+          size="sm"
+          aria-label="Toggle color theme"
+          className={colorMode === "light" ? styles.sunRise : styles.moonUp}
+        >
           {colorMode === "light" ? <LuSun /> : <LuMoon />}
         </IconButton>
-        <Text> {colorMode === "light" ? "Light Mode" : "Dark Mode"}</Text>
+        <Text>{colorMode === "light" ? "Light Mode" : "Dark Mode"}</Text>
       </ClientOnly>
     </Flex>
   );
