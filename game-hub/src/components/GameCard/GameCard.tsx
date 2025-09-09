8d54def57e64b82fdc60963c22a7bbf82d287dfe
Shahab Faderani <s.faderani@avm.de>
2025-09-09 19:41:41 +0200

    FIX: improve game card focus and hover states

--- game-hub/src/components/GameCard/GameCard.tsx
+++ game-hub/src/components/GameCard/GameCard.tsx
@@ -1,4 +1,4 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import { Game } from "../../hooks/useGames";
 import styles from "./GameCard.module.css";
 import { useColorMode } from "../../components/ui/color-mode";
@@ -15,18 +15,16 @@ const GameCard = ({ game }: Props) => {
 
   const cardClass = colorMode === "light" ? styles.lightCard : styles.darkCard;
   const bodyClass = colorMode === "light" ? styles.lightBody : styles.darkBody;
-  const headingClass =
-    colorMode === "light" ? styles.lightHeading : styles.darkHeading;
 
   return (
     <div className={`${styles.card} ${cardClass}`} tabIndex={0}>
-      <img
-        src={getCroppedImageUrl( game.background_image)}
+      <Image
+        src={getCroppedImageUrl(game.background_image)}
         alt={game.name}
         className={styles.image}
       />
-      <div className={bodyClass}>
-        <h3 className={headingClass}>{game.name}</h3>
+      <div className={`${styles.body} ${bodyClass}`}>
+        <h3 className={styles.heading}>{game.name}</h3>
         <HStack justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
