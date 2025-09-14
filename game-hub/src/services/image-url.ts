51519f34316c5197465059c93db55bb991b8fb0b
Shahab Faderani <s.faderani@avm.de>
2025-09-14 12:42:47 +0200

    FIX: handle games with no image

--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url: string, width: number = 600, height: number = 400) => {
-  if (!url) return '';
+  if (!url) return noImage;
     const target ='media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + `crop/${width}/${height}/` + url.slice(index);
